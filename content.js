fb843f7e3b11a384512199b3b9459a0d0dd5e3a3
JRH89 <thegemlins@gmail.com>
2024-07-25 16:56:58 -0700

    better automation of skip button click.

--- content.js
+++ content.js
@@ -1,13 +1,12 @@
-
+// Function to click the "Skip Ad" button if it appears and is visible
 function clickSkipAdButton() {
-    console.log('Checking for skip button...');
-
-    // Use the correct CSS selector for the "Skip Ad" button
     const skipButton = document.querySelector('.ytp-skip-ad-button');
 
     if (skipButton && isElementVisible(skipButton)) {
-        skipButton.click();
+        setTimeout(() => {
+            skipButton.click();
 
+        }, getRandomDelay());
     }
 }
 
@@ -15,6 +14,11 @@ function clickSkipAdButton() {
 function isElementVisible(element) {
     const style = window.getComputedStyle(element);
     return style.display !== 'none' && style.visibility !== 'hidden' && style.opacity !== '0';
+}
+
+// Function to get a random delay between 1 and 3 seconds
+function getRandomDelay() {
+    return Math.floor(Math.random() * 2000) + 1000;
 }
 
 // Run the function every second to check for the "Skip Ad" button
